2a164784ccf01f72d331c7f91400243ae32970aa
GRRebeka <36545021+GRRebeka@users.noreply.github.com>
2018-04-23 22:40:46 +0200

    Update beadando_EX10.py

--- beadando_EX10.py
+++ beadando_EX10.py
@@ -1,21 +1,22 @@
 
 ###2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? ###
 
-import time
-
 def osztas():
     db=0
-    szam = 10
-    while db!=10:
-        for i in range(1,10):
+    szam = 20
+    while db!=20:
+        for i in range(1,21):
             if szam%i==0:
                 db+=1
-        if db==10:
+        if db==20:
             return szam
+        else:
+            db=0
         szam+=1
 
-print(osztas())
+def main():
+    print(osztas())
 
-
+main()
 
 
