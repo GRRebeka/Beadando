30ce042a873d0075bcea543df48c5f0f9cc0efa4
GRRebeka <36545021+GRRebeka@users.noreply.github.com>
2018-04-23 21:04:40 +0200

    Update beadando_EX9.py

--- beadando_EX9.py
+++ beadando_EX9.py
@@ -1,6 +1,6 @@
 
 ###The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143 ?###
-###A 13195 elsődleges tényezői: 5, 7, 13 és 29. Mi a 600851475143-as szám legfontosabb tényezője?###
+
 
 
 def prim_szamok():
