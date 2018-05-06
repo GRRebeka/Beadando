ba030d5013f2353301c30e50d7718f2f482bad83
GRRebeka <36545021+GRRebeka@users.noreply.github.com>
2018-05-06 21:29:46 +0200

    Update beadando_EX9.py

--- beadando_EX9.py
+++ beadando_EX9.py
@@ -3,8 +3,34 @@
 
 
 
-def prim_szamok():
-    prim_lst=[]
+def prim_e(szam):
+    db=1
+    if szam==2:
+            return 'Prim'
+    for i in range(2,szam):
+        if szam%i == 0:
+            db+=1
+            if db>2:
+                return 'Nem prim'
+    return 'Prim'
+
+def EX9(number):
+
+    primek_is = []
+
+    for i in range(2,number):
+        if number%i==0:
+            if prim_e(i)=='Prim':
+                primek_is.append(i)
+ 
+    max = 0
+    for pmax in primek_is:
+        if pmax>max:
+            max = pmax
+    print('{} tha largest prim'.format(max))
+
+
+EX9(600851475143)
 
 
 
