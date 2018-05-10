745cb75a8d183ab80d6ce9e00a46ef4633fa46bb
GRRebeka <36545021+GRRebeka@users.noreply.github.com>
2018-05-10 17:21:29 +0200

    Update beadando_EX9.py

--- beadando_EX9.py
+++ beadando_EX9.py
@@ -14,16 +14,12 @@ def prim_e(szam):
 
 def EX9(number):
 
-    primek_is = []
     max=0
     for i in range(2,number):
         if number%i==0:
             if prim_e(i)=='Prim':
-                primek_is.append(i)
                 if i>max:
-                    max=i
-                print(primek_is)
-
+                    max=i       
     print('{} the largest prim'.format(max))
 
 EX9(600851475143)
